8dfc8ca35b28b27a41599d5b192e01b3d403dca4
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2016-10-24 11:01:30 +0100

    Update split5.py

--- split5.py
+++ split5.py
@@ -1,3 +1,12 @@
+#Divides the matrix row-wise. 
+#Uses SCATTERV. 
+#The programmers have to compute the distribution of data. 
+#One process can have several rows. 
+#One process can have more rows than others. 
+#The data can have gaps (if the programmer desires that). Each process receives the data (several rows) in a array of 1DIMENSION.
+
+#mpiexec -n 10 python split5.py > output_5
+
 from mpi4py import MPI
 import numpy
 from numpy import random
